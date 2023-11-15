c60e09ae42c7bf826349f2ed645dd4d37e949a6b
arfleander <arfleander10@gmail.com>
2023-11-15 00:43:17 -0300

    criacao de login

---
+++ Eco_Solution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void main(){
+    login();
+}
+
+void login(){
+    char autent;
+
+    do{
+        printf("\nPossui login cadastrado? (S/N)\n");
+        scanf(" %c", &autent);
+
+        /*transforma o caractere em maiúsculo*/
+        autent = toupper(autent);
+
+        /*verifica a reposta do usuario*/
+        if(autent == 'S'){
+            usuariocadastrado();
+
+        }else if(autent == 'N'){
+            cadastrarusuario();
+
+        }else{
+            printf("\nResposta invalida! Tente novamente.\n");
+        }
+    }while(autent != 'S' && autent != 'N');
+}
