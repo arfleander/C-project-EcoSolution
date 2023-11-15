6646f0e17ed7c2e7751e01939856efdb2589f7c6
arfleander <arfleander10@gmail.com>
2023-11-15 00:48:39 -0300

    implementacao do login

--- Eco_Solution.c
+++ Eco_Solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 void main(){
     login();
@@ -26,4 +27,57 @@ void login(){
             printf("\nResposta invalida! Tente novamente.\n");
         }
     }while(autent != 'S' && autent != 'N');
+}
+
+void usuariocadastrado(char* novo_user,  char* nova_senha){
+    char user[31], senha[21];
+
+    do{
+        printf("\nInforme o seu usuario (max 30): ");
+        scanf("%30s", user);
+
+        if(strcmp(user, novo_user) == 0){
+            printf("\nDigite sua senha (max 20): ");
+            scanf("%20s", senha);
+
+            if(strcmp(senha, nova_senha) == 0){
+                printf("\nOla %s. Bem-vindo ao menu inicial da Eco Solution!\n", user);
+                break;
+
+            }else{
+                printf("\nSenha incorreta. Tente novamente");
+            }
+
+        }else{
+            printf("\nUsuario incorreto. Tente novamente!");
+
+        }
+    }while(1);
+}
+
+void cadastrarusuario(){
+    char novo_user[31], nova_senha[21];
+
+    printf("\nDigite um nome de usuario (maximo de 30 caracteres): ");
+    scanf("%30s", novo_user);
+
+    do{
+        if(strlen(novo_user) != 0){
+
+            printf("\nDigite uma senha (maximo de 20 caracteres): ");
+            scanf("%20s", nova_senha);
+
+            if(strlen(nova_senha) != 0){
+
+                printf("\nUsuario criado com sucesso!\n");
+                break;
+            }else{
+                printf("\nSenha invalida. Tente novamente!");
+            }
+        }else{
+            printf("\nUsuario invalido. Tente Novamente!");
+        }
+    }while(strlen(novo_user) != 0 && strlen(nova_senha) != 0);
+
+    usuariocadastrado(novo_user, nova_senha);
 }
